03b30cd75ee149256b3d6594b0072ae4af099adf
chou1022 <160296003+chou1022@users.noreply.github.com>
2024-05-02 11:00:17 +0800

    exit

---
+++ exit/data.js
@@ -0,0 +1,403 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-m1",
+      "name": "M1出入口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3688612793839674,
+          "pitch": 0.3518792075362711,
+          "rotation": 4.71238898038469,
+          "target": "3-m5"
+        },
+        {
+          "yaw": -0.2570014251893191,
+          "pitch": 0.4283799427501709,
+          "rotation": 3.9269908169872414,
+          "target": "2-m3"
+        },
+        {
+          "yaw": -0.09037568976563293,
+          "pitch": 0.4776252617663719,
+          "rotation": 3.141592653589793,
+          "target": "5-m7"
+        },
+        {
+          "yaw": 0.0698869659679886,
+          "pitch": 0.39511077428365127,
+          "rotation": 2.356194490192345,
+          "target": "1-m2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-m2",
+      "name": "M2出入口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.31817396563749156,
+          "pitch": 0.13508776180541915,
+          "rotation": 4.71238898038469,
+          "target": "0-m1"
+        },
+        {
+          "yaw": -0.26794500442783153,
+          "pitch": 0.25729627235427266,
+          "rotation": 22.776546738526015,
+          "target": "3-m5"
+        },
+        {
+          "yaw": -0.1807425761027872,
+          "pitch": 0.3622243112042227,
+          "rotation": 3.9269908169872414,
+          "target": "4-m6"
+        },
+        {
+          "yaw": -0.06857812397593044,
+          "pitch": 0.4744963244765046,
+          "rotation": 3.141592653589793,
+          "target": "2-m3"
+        },
+        {
+          "yaw": 0.113146125808262,
+          "pitch": 0.4911316828782937,
+          "rotation": 3.141592653589793,
+          "target": "5-m7"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-m3",
+      "name": "M3出入口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.06362353961942446,
+          "pitch": 0.2524301535151139,
+          "rotation": 0,
+          "target": "0-m1"
+        },
+        {
+          "yaw": 0.2800755664245287,
+          "pitch": 0.2881005646365633,
+          "rotation": 0.7853981633974483,
+          "target": "1-m2"
+        },
+        {
+          "yaw": 0.37463372092052794,
+          "pitch": 0.4339732846957016,
+          "rotation": 1.5707963267948966,
+          "target": "5-m7"
+        },
+        {
+          "yaw": 0.2223424655020363,
+          "pitch": 0.6321865786545366,
+          "rotation": 3.141592653589793,
+          "target": "6-m8"
+        },
+        {
+          "yaw": -0.0012156416082778065,
+          "pitch": 0.5567098283646441,
+          "rotation": 3.9269908169872414,
+          "target": "4-m6"
+        },
+        {
+          "yaw": -0.13772585206960208,
+          "pitch": 0.37488358078319806,
+          "rotation": 4.71238898038469,
+          "target": "3-m5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-m5",
+      "name": "M5出入口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "rotation": 0.7853981633974483,
+          "target": "0-m1"
+        },
+        {
+          "yaw": 0.16635146199138973,
+          "pitch": 0.08404666984068498,
+          "rotation": 1.5707963267948966,
+          "target": "2-m3"
+        },
+        {
+          "yaw": 0.10273535843312942,
+          "pitch": 0.1853757176862061,
+          "rotation": 2.356194490192345,
+          "target": "4-m6"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-m6",
+      "name": "M6出入口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.1288907356663067,
+          "pitch": 0.04379045956324745,
+          "rotation": 5.497787143782138,
+          "target": "3-m5"
+        },
+        {
+          "yaw": 0.09981986158097378,
+          "pitch": 0.257915072794189,
+          "rotation": 1.5707963267948966,
+          "target": "6-m8"
+        },
+        {
+          "yaw": 0.10856123344754565,
+          "pitch": 0.0934335642466202,
+          "rotation": 0.7853981633974483,
+          "target": "2-m3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-m7",
+      "name": "M7出入口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.026504968977551613,
+          "pitch": 0.1548172833947703,
+          "rotation": 0,
+          "target": "1-m2"
+        },
+        {
+          "yaw": -0.13178670962372507,
+          "pitch": 0.19601866116965105,
+          "rotation": 5.497787143782138,
+          "target": "0-m1"
+        },
+        {
+          "yaw": -0.23421987910945674,
+          "pitch": 0.3000998853760741,
+          "rotation": 4.71238898038469,
+          "target": "2-m3"
+        },
+        {
+          "yaw": -0.08814101039961919,
+          "pitch": 0.43652548691603954,
+          "rotation": 3.9269908169872414,
+          "target": "6-m8"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-m8",
+      "name": "M8出入口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.1948554644167082,
+          "pitch": 0.30516205012253295,
+          "rotation": 4.71238898038469,
+          "target": "4-m6"
+        },
+        {
+          "yaw": 0.011782119583941153,
+          "pitch": 0.11437167319787456,
+          "rotation": 0,
+          "target": "2-m3"
+        },
+        {
+          "yaw": 0.1346805496993717,
+          "pitch": 0.20435538194129066,
+          "rotation": 0.7853981633974483,
+          "target": "5-m7"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
